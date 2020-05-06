a796c3a6c8f67c61a90d2a3f2e1e14f50ec91b0a
Viktor <smirn04@yandex.ru>
2020-05-06 15:04:04 +0300

    Initial Commit

---
+++ Список продуктов/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Список продуктов
+//
+//  Created by Viktor Smirnov on 06.05.2020.
+//  Copyright © 2020 Viktor Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
